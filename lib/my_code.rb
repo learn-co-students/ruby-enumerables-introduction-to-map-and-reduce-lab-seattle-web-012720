5d3052625eb46935a54161bd751d23a8a5436025
Clarion Heard <clarion.heard@gmail.com>
2020-01-04 16:31:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array.map{|x|-x}
+end
+
+def map_to_no_change(source_array)
+  source_array.map{|x|x}
+end
+
+def map_to_double(source_array)
+  source_array.map{|x|2*x}
+end
+
+def map_to_square(source_array)
+  source_array.map{|x|x**2}
+end
+
+def reduce_to_total(source_array, starting_point=0)
+    source_array.sum(starting_point)
+end
+
+def reduce_to_all_true(source_array)
+  source_array.all?
+end
+
+def reduce_to_any_true(source_array)
+  source_array.any?
+end
