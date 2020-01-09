d32c3c094378da8d4c7306aa0120b41e0f3c3259
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-09 20:21:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,77 @@
-# My Code here....
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+  new << -source_array[i] 
+  i += 1
+  end
+  new
+end
+
+def map_to_no_change(source_array)
+  new = []
+  new = source_array
+  new
+end
+
+def map_to_double(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new << source_array[i] *2 
+    i += 1
+  end
+  new
+end
+
+def map_to_square(source_array)
+  new =[]
+  i = 0
+  while i < source_array.length do
+    new << source_array[i] **2
+    i += 1
+  end
+  new
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  i = 0
+  while i < source_array.length
+ total += source_array[i] 
+  i += 1
+end
+total
+end
+
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+   return false if !source_array[i]
+    i += 1
+  end
+    true
+  end
+
+  
+def reduce_to_any_true(source_array)  
+ i = 0
+  while i < source_array.length do
+   return true if source_array[i]
+    i += 1
+  end
+    false
+  end
+  
+
+
+
+
+
+
+
+
+
+  
+  
